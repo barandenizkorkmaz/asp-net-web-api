5820d7886c7dca07ce32166ff9e59c211fa5a534
Baran Deniz Korkmaz <barandenizkorkmaz@gmail.com>
2024-03-18 13:38:25 +0100

    add seeding mechanism

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,4 +1,5 @@
 using Restaurants.Infrastructure.Extensions;
+using Restaurants.Infrastructure.Seeders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,13 @@ builder.Services.AddControllers();
 builder.Services.AddInfrastructure(builder.Configuration); // Extension method. See references in the comments.
 
 var app = builder.Build();
+
+// Seed your application
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
+
+await seeder.Seed();
 
 // Middleware
 
