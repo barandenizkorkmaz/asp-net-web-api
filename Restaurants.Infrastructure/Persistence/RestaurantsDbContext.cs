ab318a111cdc468518d35a29028c65004faa8358
Baran Deniz Korkmaz <barandenizkorkmaz@gmail.com>
2024-03-18 10:07:42 +0100

    add migration set up

---
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence;
+
+internal class RestaurantsDbContext : DbContext
+{
+    internal DbSet<Restaurant> Restaurants { get; set;}
+    internal DbSet<Dish> Dishes { get; set;}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=RestaurantsDb;Trusted_Connection=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Restaurant>()
+            .OwnsOne(r => r.Address); // One-to-one relationship
+
+        modelBuilder.Entity<Restaurant>()
+            .HasMany(r => r.Dishes) // One-to-many relationship (one restaurant to many dishes)
+            .WithOne() // Many-to-one relationship from dish side (many dishes to one restaurant)
+            .HasForeignKey(d => d.RestaurantId); // One-to-many relationship
+
+    }
+}
