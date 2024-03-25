f956453e87b0f6e4fbe9885b5b1f0bf83778e6a0
Baran Deniz Korkmaz <barandenizkorkmaz@gmail.com>
2024-03-25 17:37:50 +0100

    s7: configure serilog for logging to file

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -20,9 +20,7 @@ builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration); // Extension method. See references in the comments.
 builder.Host.UseSerilog((context, configuration) =>
     configuration
-        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning) // To suppress Microsoft info logs
-        .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
-        .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}") // Configure default outputTemplate
+        .ReadFrom.Configuration(context.Configuration)
 );
 
 var app = builder.Build();
