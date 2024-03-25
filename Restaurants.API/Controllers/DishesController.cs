32dcf2f1501142b9e047eea572b8da25124cec38
Baran Deniz Korkmaz <barandenizkorkmaz@gmail.com>
2024-03-26 00:07:37 +0100

    s8: add create dish endpoint

---
+++ Restaurants.API/Controllers/DishesController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Dishes.Commands.CreateDish;
+
+namespace Restaurants.API.Controllers;
+
+[Route("api/restaurant/{restaurantId}/dishes")]
+[ApiController]
+public class DishesController(IMediator mediator) : ControllerBase
+{
+    // observe that restaurantId is a route paramater in controller-level
+    [HttpPost]
+    public async Task<IActionResult> CreateDish([FromRoute] int restaurantId, CreateDishCommand command)
+    {
+        command.RestaurantId = restaurantId;
+        await mediator.Send(command);
+        return Created();
+    }
+}
--- Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -30,6 +30,7 @@ public static class ServiceCollectionExtensions
 
         services.AddScoped<IRestaurantSeeder, RestaurantSeeder>();
         services.AddScoped<IRestaurantsRepository, RestaurantsRepository>();
+        services.AddScoped<IDishesRepository, DishesRepository>();
 
     }
 }
