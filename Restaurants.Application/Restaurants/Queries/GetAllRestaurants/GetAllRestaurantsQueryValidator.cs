cfd37da13367832ee60be62f2532e15c9b51b97c
Baran Deniz Korkmaz <barandenizkorkmaz@gmail.com>
2024-04-07 21:19:55 +0200

    s11: add pagination request validation for get all restaurants endpoint

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
+
+public class GetAllRestaurantsQueryValidator : AbstractValidator<GetAllRestaurantsQuery>
+{
+    private readonly int[] allowedPageSizes = [5, 10, 25, 50];
+
+    public GetAllRestaurantsQueryValidator()
+    {
+        RuleFor(r => r.PageNumber).GreaterThanOrEqualTo(1);
+        RuleFor(r => r.PageSize)
+            .Must(value => allowedPageSizes.Contains(value))
+            .WithMessage($"Page size must be in [{string.Join(",", allowedPageSizes)}]");
+    }
+}
